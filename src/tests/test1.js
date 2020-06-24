ab2541be738466101a0226f4129ec188337607d3
Pupperbot <pupperbot@protonmail.com>
2020-06-24 17:21:19 -0600

    Updated typo

--- src/tests/test1.js
+++ src/tests/test1.js
@@ -5,7 +5,7 @@ const TestHook = () => {
 // day to day basis. There will be 3 tasks, ranging from easy to hard. We expect you to be able to solve all 3 in one go, but obviously we understand
 // any sort of challenges that may come up. 
 
-// Here is the first task! Using hooks, please create a setState named name, and setName. Also, please make a useEffect statement that sets your name to 'Joe'.
+// Here is the first task! Using hooks, please create a useState named name, and setName. Also, please make a useEffect statement that sets your name to 'Joe'.
 // Also, create a button that updates the state to something else post render!
 // Please display it in any way you would like!,
 
@@ -15,6 +15,5 @@ return (
   </div>
   )
 }
-
 
 export default TestHook;
